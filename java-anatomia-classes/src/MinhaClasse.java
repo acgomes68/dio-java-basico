25e0ac1f3fa64d25b2376b7345886dddb1116f07
Antonio Carlos Gomes <acgomes68@pm.me>
2024-05-13 16:48:29 -0300

    Anatomia das classes 3

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -1,7 +1,14 @@
 public class MinhaClasse {
 
     public static void main(String[] args) {
-        System.out.print("Olá turma, sejam bem-vindos");
+        String primeiroNome = "Antonio";
+        String segundoNome = "Gomes";
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
     }
     
+    public static String nomeCompleto(String primeiroNome, String segundoNome) {
+        return primeiroNome.concat(" ").concat(segundoNome);
+    }
 }
