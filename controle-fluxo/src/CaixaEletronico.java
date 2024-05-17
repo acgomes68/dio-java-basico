1b17cb16324637591ebc19d3040513825e4a0db0
Antonio Carlos Gomes <acgomes68@pm.me>
2024-05-17 13:09:08 -0300

    Otimização do operador de atualização do saldo

--- controle-fluxo/src/CaixaEletronico.java
+++ controle-fluxo/src/CaixaEletronico.java
@@ -6,7 +6,7 @@ public class CaixaEletronico {
        double valorSolicitado = 17.0;
 
        if(valorSolicitado < saldo)
-        saldo = saldo - valorSolicitado;
+        saldo -= valorSolicitado;
 
         System.out.println(saldo);
 
