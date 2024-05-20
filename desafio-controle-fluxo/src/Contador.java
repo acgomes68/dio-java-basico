fd21c78b7ad5f10bc00861b18c99eef2d9b70b2f
Antonio Carlos Gomes <acgomes68@pm.me>
2024-05-20 20:07:39 -0300

    Ajuste da lógica de modo a imprimir a quantidade de iterações e não a faixa de números selecionada

--- desafio-controle-fluxo/src/Contador.java
+++ desafio-controle-fluxo/src/Contador.java
@@ -22,13 +22,12 @@ public class Contador {
 	}
 	static void contar(int parametroUm, int parametroDois ) throws ParametrosInvalidosException {
 		//validar se parametroUm é MAIOR que parametroDois e lançar a exceção
+		if(parametroUm > parametroDois) throw new ParametrosInvalidosException();
 		
 		int contagem = parametroDois - parametroUm;
+
 		//realizar o for para imprimir os números com base na variável contagem
-
-        if(contagem < 0) throw new ParametrosInvalidosException();
-
-        for (int intContador = parametroUm; intContador < parametroDois; intContador++) {
+        for (int intContador = 1; intContador <= contagem; intContador++) {
             System.out.println("Imprimindo o número " + intContador);
         }
 	}
