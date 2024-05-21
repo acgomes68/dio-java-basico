0b02b2b083000dde7030e0daab1fd3ef32fd7122
Antonio Carlos Gomes <acgomes68@pm.me>
2024-05-21 18:50:20 -0300

    Alteração da classe Escola usando os getters e setters da classe Aluno

--- getters-setters/src/escola/Escola.java
+++ getters-setters/src/escola/Escola.java
@@ -3,11 +3,9 @@ package escola;
 public class Escola {
 	public static void main(String[] args) {
 		Aluno felipe = new Aluno();
-		felipe.nome="Felipe";
-		felipe.idade = 8;
+		felipe.setNome("Felipe");
+		felipe.setIdade(8);
 		
-		System.out.println("O aluno " + felipe.nome + " tem " + felipe.idade + " anos ");
-		//RESULTADO NO CONSOLE
-		//O aluno Felipe tem 8 anos 		
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");	
 	}
 }
